5e8ccc8abd872dc3a3a385bc3a9e866b6661443a
Nattaphon_EnterHelloWorld <natlove.natlove202@gmail.com>
2018-02-04 09:56:38 +0700

    testa

--- tests/AITest.php
+++ tests/AITest.php
@@ -22,65 +22,6 @@ final class AITest extends TestCase
         $expected_result = 'Female';
         $this->assertEquals($expected_result, $result);
     }
-    public function testGender_Male3(): void
-    {
-        $result = AI::getGender('ผม');
-        $expected_result = 'Female';
-        $this->assertEquals($expected_result, $result);
-    }
-    public function testGender_Female(): void
-    {
-        $result = AI::getGender('สวัสดีค่ะ');
-        $expected_result = 'Female';
-        $this->assertEquals($expected_result, $result);
-    }
-    public function testGender_Female1(): void
-    {
-        $result = AI::getGender('ฉัน');
-        $expected_result = 'Female';
-        $this->assertEquals($expected_result, $result);   
-    }
-    public function testSentiment_positive(): void
-    {
-        $result = AI::getSentiment('สนุก');
-        $expected_result = 'Positive';
-        $this->assertEquals($expected_result, $result);
-    }
-    public function testSentiment_positive1(): void
-    {
-        $result = AI::getSentiment('เบื่อ');
-        $expected_result = 'Negative';
-        $this->assertEquals($expected_result, $result);
-    }
-    public function testSentiment_positive2(): void
-    {
-        $result = AI::getSentiment('เฉยๆ');
-        $expected_result = 'Neutral';
-        $this->assertEquals($expected_result, $result);
-    }
-    public function testrudeword()): void
-    {
-        $result = AI::getRudeWords('Fuck');
-        $expected_result = 'Neutral';
-        $this->assertEquals($expected_result, $result);
-    }
-    public function testrudeword1()): void
-    {
-        $result = AI::getRudeWords('Fuck');
-        $expected_result = 'Neutral';
-        $this->assertEquals($expected_result, $result);
-    }
-    public function testrudeword2()): void
-    {
-        $result = AI::getLanguages('Fuck');
-        $expected_result = 'EN';
-        $this->assertEquals($expected_result, $result);
-    }
-    public function testrudeword3()): void
-    {
-        $result = AI::getLanguages('วู้');
-        $expected_result = 'TH';
-        $this->assertEquals($expected_result, $result);
-    }
+   
 
 }
